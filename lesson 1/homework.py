7bea15d243fd6259cad1875244e034e773a0b7a6
mon1ycold <your.actual.email@example.com>
2026-06-19 22:22:22 +0500

    Upload Lesson 1 folder

---
+++ lesson 1/homework.py
@@ -0,0 +1,86 @@
+#1.misol
+print("Ahror")
+#2.misol
+print("It is Mon1ycold")
+#3.misol
+print("Salom")
+#4.Misol
+print(" Yoshim 20 yoshda")
+#5.Misol
+print(" a = 5 , b =10, c=15")
+#6.Misol
+print("hello 'hello' 'hi' how are you'")
+#7.Misol
+print("Menig ismim 'Ali'")
+#8.Misol
+print("2+3")
+#9.Misol
+print("*")
+#10.Misol
+print("***")
+#11.Misol 
+print("*")
+#12.Misol
+print("*")
+print("*")
+print("*")
+#13.Misol
+print("*")
+print("**")
+print("***")
+#14.Misol
+print("1")
+print("2")
+print("3")
+#15.Misol
+print("#####")
+#16.Misol
+print("+++")
+print("+++")
+print("+++")
+#17.Misol
+print("*")
+print("**")
+print("***")
+print("****")
+print("*****")
+#18.Misol
+print("1")
+print("12")
+print("123")
+#19.Misol
+print("****")
+print("***")
+print("**")
+print("*")
+#20.Misol
+print("@@@@")
+print("@@@@")
+#21.Misol
+print("* * * *")
+#22.Misol
+print("*")
+print("**")
+print("***")
+print("**")
+print("*")
+#23.Misol
+print("#")
+print("@")
+print("*")
+print("+")
+#24.Misol
+print ("  ^")
+print (" / |")
+print("____")
+print("|   |")
+print("____")
+#25.Misol
+print("         /\         ")
+print("        /  \        ")
+print("       /    \     ")
+print("      /      \     ")
+print("     /________\    ")
+print("     |        |     ")
+print("     |  HOUSE |     ")
+print("     |________|     ")
---
+++ lesson 1/python.py
@@ -0,0 +1,42 @@
+#funksiyalar 
+#print("Hello world!") 
+#print("MON1YCOLD")
+#print("itzreal")
+#print("i go to school")
+
+
+
+#1. Misol 
+print ("Assalomu aleykum!")
+#2. Misol
+print("2024")
+print("45")
+#3.Misol
+print("100")
+#4.Misol
+print("5.0")
+#5.Misol
+print("Salom dunyo Python!")
+#6.Misol
+print(" Dasturlash tilini o'rganamiz")
+#7.Misol
+print("Mening ismim Dildora, yoshim 25 da")
+#8.Misol
+print("5+3=8")
+#9.Misol
+print("")
+#10.Misol
+print("Men dasturlash tilini o'rganmoqdaman" )
+#11.Misol
+print("Bu birinchi qator ")
+print("Bu ikkinchi qator")
+#12.Misol
+print("Python")
+print("Django")
+print(" Telegram Bot")
+#13.misol
+print("***********************")
+print("Backend kursi")
+print("***********************")
+#14.misol
+print("Bu birichi qator. Bu Ikkinchi qator")
